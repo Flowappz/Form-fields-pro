873950cbaa797ed9d3d758cac0b9948d3e6354d1
rabbykhairul <rabbykhairul@gmail.com>
2023-09-20 17:48:13 +0600

    method to remove all childs of a html element

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-document.getElementById("lorem").onsubmit = async (event) => {
-  event.preventDefault()
-  const el = await webflow.getSelectedElement();
-  if (el && el.textContent) {
-    el.setTextContent(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
-        "eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    )
-    el.save()
+function removeAllChildFromElement(element: HTMLElement) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
 }
+
+document.getElementById("lorem").onsubmit = async (event) => {
+  event.preventDefault();
+
+  const el = await webflow.getSelectedElement();
+};
