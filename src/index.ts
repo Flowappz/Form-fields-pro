f036fdaaf82d9ef9ac081633ed435600db80a07b
rabbykhairul <rabbykhairul@gmail.com>
2023-09-20 17:59:30 +0600

    logic to detect if the current selected webflow elment is a form or not

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,26 @@ function removeAllChildFromElement(element: HTMLElement) {
   }
 }
 
+function displayFormSelectionError(element: AnyElement) {
+  const elementInfoBox = document.getElementById("selectedElementInfo");
+  removeAllChildFromElement(elementInfoBox);
+
+  elementInfoBox.appendChild(document.createTextNode(`Please select a form elment to continue! Your selected element type is ${element.type}`));
+}
+
+function displayCorrectSelection(element: AnyElement) {
+  const elementInfoBox = document.getElementById("selectedElementInfo");
+  removeAllChildFromElement(elementInfoBox);
+
+  elementInfoBox.appendChild(document.createTextNode(`OK! Your selected element type is ${element.type}`));
+}
+
 document.getElementById("lorem").onsubmit = async (event) => {
   event.preventDefault();
 
   const el = await webflow.getSelectedElement();
+
+  if (!(el.type === "FormWrapper" || el.type === "FormForm")) {
+    displayFormSelectionError(el);
+  } else displayCorrectSelection(el);
 };
