3ef069f0bb6be7c829c82002464b6a5deedab566
rabbykhairul <rabbykhairul@gmail.com>
2023-09-22 00:20:53 +0600

    add prompt to select element

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button>Selected el type is {selectedElement?.type}</button>
+        <button style={{ color: "gold" }}>Selected el type is {selectedElement?.type}</button>
 
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>Select an element</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
