3c14a0d68528582dd6a918c9165b592c3a43b9b7
rabbykhairul <rabbykhairul@gmail.com>
2023-09-22 21:39:03 +0600

    home view, show the currently selected elementy type

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,21 @@
+import { PropsWithChildren } from "react";
 import "../styles/Home.css";
 
-export default function Home() {
+type props = {
+  selectedElement: AnyElement | null;
+};
+
+export default function Home({ selectedElement }: PropsWithChildren<props>) {
   return (
     <div className="home-container p-50">
-      <div className="d-flex flex-align-center p-20">
+      <div className="d-flex flex-align-center p-20" style={{ width: "70%" }}>
         <h1 className="text-decoration-underline">Form Fields Pro</h1>
       </div>
       <div className="d-flex flex-align-center p-20">
         <div className="card" style={{ background: "darkslategrey" }}>
-          <p>Please select a form element in the designer to continue...</p>
+          <p>
+            Your current selected element type is <strong>{selectedElement?.type}</strong>. Please select a form element in the designer to continue.
+          </p>
         </div>
       </div>
     </div>
