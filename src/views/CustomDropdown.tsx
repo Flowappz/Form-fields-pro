f50fc759afe854ead92e592ed836a0949edb9e4f
rabbykhairul <rabbykhairul@gmail.com>
2023-09-22 22:04:14 +0600

    allow user to input dropdown label, items

--- src/views/CustomDropdown.tsx
+++ src/views/CustomDropdown.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export default function CustomDropdown() {
-  return <h1>custom dropdown</h1>;
+  const [dropdownLabel, setDropdownLabel] = useState("");
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState<string[]>([]);
+
+  const handleAddItem = () => {
+    setItems([item, ...items]);
+    setItem("");
+  };
+
+  return (
+    <div>
+      <label htmlFor="dropdownLabel" style={{ display: "block" }}>
+        Enter the dropdown title
+      </label>
+      <input type="text" value={dropdownLabel} onChange={(e) => setDropdownLabel(e.target.value)} />
+
+      <br />
+      <input type="text" value={item} onChange={(e) => setItem(e.target.value)} />
+      <button onClick={handleAddItem}>Add item</button>
+
+      <br />
+      <h4>Dropdown Items:</h4>
+      <ul>
+        {items.map((i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
