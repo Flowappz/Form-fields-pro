4cc995fb5a0cc5dc73a32f3acff1735e423af754
rabbykhairul <rabbykhairul@gmail.com>
2023-10-04 10:39:59 +0600

    push custom dropdown script to the webflow site

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./views/Home";
 import CustomDropdown from "./views/CustomDropdown";
+import axios from "axios";
 
 declare global {
   interface Window {
@@ -14,6 +15,20 @@ enum Views {
   CUSTOM_DROPDOWN,
 }
 
+interface IPushScriptApiResponse {
+  data: {
+    siteId: string;
+    status: "OK";
+    script: {
+      id: string;
+      displayName: string;
+      version: string;
+      location: "header" | "footer";
+      attributes: { [key: string]: string };
+    };
+  };
+}
+
 function App() {
   const [selectedelement, setSelectedElement] = useState<AnyElement | null>(null);
   const [formElement, setFormElement] = useState<FormFormElement | FormWrapperElement | null>(null);
@@ -21,6 +36,33 @@ function App() {
 
   useEffect(function listenToElementSelectonChange() {
     window._myWebflow.subscribe("selectedelement", (element) => setSelectedElement(element));
+  }, []);
+
+  useEffect(function pushCustomDropdownScript() {
+    const pushScript = async () => {
+      try {
+        const { siteId } = await window._myWebflow.getSiteInfo();
+
+        const { data } = await axios.post<IPushScriptApiResponse>(
+          `${import.meta.env.VITE_DATA_CLIENT_URL}/app/attach-custom-script`,
+          {
+            siteId,
+            scriptName: "dropdown",
+          },
+          {
+            headers: {
+              Authorization: `Basic ${import.meta.env.VITE_BASIC_AUTH_TOKEN}`,
+            },
+          }
+        );
+
+        console.log("\n\nSuccessfully attached custom dropdown script: ", data, "\n\n");
+      } catch (err) {
+        console.log("Error pushing custom dropdown script", err);
+      }
+    };
+
+    pushScript();
   }, []);
 
   useEffect(
