851f2f7d0d162e8075e233424da9e0f33a8f1d70
rabbykhairul <rabbykhairul@gmail.com>
2023-10-06 17:53:38 +0600

    left side menu, search input markup

---
+++ src/components/menu/LeftSideMenu.tsx
@@ -0,0 +1,9 @@
+export default function LeftSideMenu() {
+  return (
+    <>
+      <div className="w-full">
+        <input type="text" placeholder="Search" className="w-full mb-2 bg-[#2B2B2B] text-[11px] leading-[16px] p-1 focus:outline-none" />
+      </div>
+    </>
+  );
+}
