a67d92710b17253a3f639c0d490a4e09b32ca52b
rabbykhairul <rabbykhairul@gmail.com>
2023-10-06 20:09:17 +0600

    add click event to menu items

--- src/components/menu/LeftSideMenu.tsx
+++ src/components/menu/LeftSideMenu.tsx
@@ -15,10 +15,19 @@ const NotAvailableMenuItem: React.FC<PropsWithChildren<{ item: MenuItem }>> = ({
   );
 };
 
-const AvailableMenuItem: React.FC<PropsWithChildren<{ item: MenuItem }>> = ({ item }) => {
-  const { label, description } = item;
+interface IMenuItemComponentProps extends PropsWithChildren {
+  item: MenuItem;
+  onClick: (id: string) => {};
+}
+
+const AvailableMenuItem: React.FC<IMenuItemComponentProps> = ({ item, onClick }) => {
+  const { id, label, description } = item;
+
   return (
-    <div className="py-2 flex justify-between items-center border-t-[1.5px] border-t-[#363636] bg-[#212121] p-3">
+    <div
+      className="py-2 flex justify-between items-center border-t-[1.5px] border-t-[#363636] bg-[#212121] p-3"
+      onClick={() => onClick(id)}
+    >
       <div className="leading-[1.15rem]">
         <h3 className="font-semibold text-[0.82rem]">{label}</h3>
         <p className="text-[0.77rem] font-light text-[#ABABAB]">{description}</p>
@@ -28,14 +37,28 @@ const AvailableMenuItem: React.FC<PropsWithChildren<{ item: MenuItem }>> = ({ it
   );
 };
 
-export default function LeftSideMenu() {
+interface MenuProps extends PropsWithChildren {
+  onClick: (id: string) => {};
+}
+
+export default function LeftSideMenu({ onClick }: MenuProps) {
   return (
     <>
       <div className="p-[0.45rem] py-2">
-        <input type="text" placeholder="Search" className="w-full px-[0.3rem] bg-[#2B2B2B] text-[0.7rem] leading-[1.1rem] p-1 focus:outline-none" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full px-[0.3rem] bg-[#2B2B2B] text-[0.7rem] leading-[1.1rem] p-1 focus:outline-none"
+        />
       </div>
 
-      {MENU_ITEMS.map((item) => (item.available ? <AvailableMenuItem key={item.id} item={item} /> : <NotAvailableMenuItem key={item.id} item={item} />))}
+      {MENU_ITEMS.map((item) =>
+        item.available ? (
+          <AvailableMenuItem key={item.id} item={item} onClick={onClick} />
+        ) : (
+          <NotAvailableMenuItem key={item.id} item={item} />
+        )
+      )}
     </>
   );
 }
