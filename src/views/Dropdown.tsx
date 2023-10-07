543284501171cf0cee0610910cd609a56dbe0237
rabbykhairul <rabbykhairul@gmail.com>
2023-10-07 08:48:04 +0600

    dropdown view, input for label & field name

--- src/views/Dropdown.tsx
+++ src/views/Dropdown.tsx
@@ -1,9 +1,16 @@
+import TextInput from "../components/form/TextInput";
+
 export default function Dropdown() {
   return (
     <div className="h-full px-20">
-      <div className="leading-[1.15rem] border-b-[1.5px] border-b-[#363636] pb-[0.35rem]">
+      <div className="leading-[1.15rem] border-b-[1.25px] border-b-[#363636] pb-[0.35rem] mb-2">
         <h3 className="font-semibold text-[0.82rem]">Dropdown</h3>
         <p className="text-[0.77rem] font-light text-[#ABABAB]">Custom dropdown menu</p>
+      </div>
+
+      <div className="border-b-[#363636] border-b-[1.25px]">
+        <TextInput label="Label" />
+        <TextInput label="Field name" />
       </div>
     </div>
   );
