38eae2b4dcfd43229c091b3f65b7fcfc65175ca0
rabbykhairul <rabbykhairul@gmail.com>
2023-10-07 08:43:57 +0600

    add placeholder props to text input component

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,8 +1,9 @@
 interface TextInputProps {
   label?: string;
+  placeholder?: string;
 }
 
-export default function TextInput({ label }: TextInputProps) {
+export default function TextInput({ label, placeholder = "Type here..." }: TextInputProps) {
   return (
     <div className="pb-2 m-0 flex flex-col gap-1">
       {label ? (
@@ -10,7 +11,7 @@ export default function TextInput({ label }: TextInputProps) {
       ) : null}
       <input
         type="text"
-        placeholder="Search"
+        placeholder={placeholder}
         className="rounded-sm w-full px-[0.3rem] bg-[#2B2B2B] text-[0.7rem] leading-[1.1rem] p-1 focus:outline-none"
       />
     </div>
