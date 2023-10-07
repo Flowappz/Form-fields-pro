bb04d4b67ff3bc69fc4c9464d1ce419544296065
rabbykhairul <rabbykhairul@gmail.com>
2023-10-07 08:01:00 +0600

    dropdown create view, title & description markup

---
+++ src/views/Dropdown.tsx
@@ -0,0 +1,10 @@
+export default function Dropdown() {
+  return (
+    <div className="h-full px-20">
+      <div className="leading-[1.15rem] border-b-[1.5px] border-b-[#363636] pb-[0.35rem]">
+        <h3 className="font-semibold text-[0.82rem]">Dropdown</h3>
+        <p className="text-[0.77rem] font-light text-[#ABABAB]">Custom dropdown menu</p>
+      </div>
+    </div>
+  );
+}
