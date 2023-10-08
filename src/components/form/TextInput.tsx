b4fce6098ed0a503971a8ca862da6baee0fef26d
rabbykhairul <rabbykhairul@gmail.com>
2023-10-08 22:30:45 +0600

    add onchange, value, name props to input component

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,9 +1,18 @@
 interface TextInputProps {
   label?: string;
   placeholder?: string;
+  name?: string;
+  value?: string;
+  onChange?: (val: string) => void;
 }
 
-export default function TextInput({ label, placeholder = "Type here..." }: TextInputProps) {
+export default function TextInput({
+  label,
+  placeholder = "Type here...",
+  name = "",
+  value = "",
+  onChange = () => {},
+}: TextInputProps) {
   return (
     <div className="pb-2 m-0 flex flex-col gap-1">
       {label ? (
@@ -12,6 +21,9 @@ export default function TextInput({ label, placeholder = "Type here..." }: TextI
       <input
         type="text"
         placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         className="rounded-sm w-full px-[0.3rem] bg-[#2B2B2B] text-[0.7rem] leading-[1.1rem] p-1 focus:outline-none"
       />
     </div>
