97e5c7126940bae91e1ba21ae9dfa7eacf1f3201
rabbykhairul <rabbykhairul@gmail.com>
2023-10-16 23:40:50 +0600

    zod schema to validate number slider form input data

--- src/views/NumberSlider.tsx
+++ src/views/NumberSlider.tsx
@@ -1,12 +1,39 @@
 import { useState } from "react";
 import TextInput from "../components/form/TextInput";
+import { ZodError, z } from "zod";
+
+const inputSchema = ({ max, min }: { max: number; min: number }) =>
+  z.object({
+    label: z.string().min(1, "Please enter a label"),
+    inputName: z.string().min(1, "Please enter the input name"),
+
+    maxRange: z
+      .number({
+        invalid_type_error: "Please enter a max range value",
+      })
+      .gt(min, "Max range should be greater than minimum range value"),
+    minRange: z
+      .number({
+        invalid_type_error: "Please enter a max range value",
+      })
+      .lt(max, "Min range should be less than maximum range value"),
+
+    defaultValue: z
+      .number({
+        invalid_type_error: "Please enter a min range value",
+      })
+      .gte(min, "Default value should be greater than or equal to minimum range value")
+      .lte(max, "Default value should be less than or equal to max range value"),
+  });
 
 export default function NumberSlider() {
   const [label, setLabel] = useState("");
   const [inputName, setInputName] = useState("");
-  const [maxRange, setMaxRange] = useState("");
-  const [minRange, setMinRange] = useState("");
-  const [defaultValue, setDefaultValue] = useState("");
+  const [maxRange, setMaxRange] = useState<number | string>("");
+  const [minRange, setMinRange] = useState<number | string>("");
+  const [defaultValue, setDefaultValue] = useState<number | string>("");
+
+  const [errors, setErrors] = useState<any>({});
 
   return (
     <div className="h-full px-20">
@@ -18,37 +45,43 @@ export default function NumberSlider() {
       </div>
 
       <div className="border-b-[#363636] border-b-[1.25px]">
+        <TextInput label="Label" value={label} name="label" onChange={setLabel} error={errors.label} />
+        <TextInput label="Field name" name="input" value={inputName} onChange={setInputName} error={errors.inputName} />
+
         <TextInput
-          label="Label"
-          value={label}
-          name="label"
-          onChange={setLabel}
-          // error={errors.dropdownLabel}
-        />
-        <TextInput
-          label="Field name"
-          name="input"
-          value={inputName}
-          onChange={setInputName}
-          // error={errors.inputFieldName}
+          label="Max range"
+          name="maxRange"
+          type="number"
+          value={maxRange}
+          onChange={(val) => setMaxRange(Number(val))}
+          error={errors.maxRange}
         />
 
-        <TextInput label="Max range" name="maxRange" type="number" value={maxRange} onChange={setMaxRange} />
-
-        <TextInput label="Min range" name="minRange" type="number" value={minRange} onChange={setMinRange} />
+        <TextInput
+          label="Min range"
+          name="minRange"
+          type="number"
+          value={minRange}
+          onChange={(val) => setMinRange(Number(val))}
+          error={errors.minRange}
+        />
 
         <TextInput
           label="Default value"
           name="defaultValue"
           type="number"
           value={defaultValue}
-          onChange={setDefaultValue}
+          onChange={(val) => setDefaultValue(Number(val))}
+          error={errors.defaultValue}
         />
       </div>
 
       <div className="mt-[0.3rem]">
         <div className="mt-2">
-          <button className="w-full bg-[#0073E6] text-center text-[0.77rem] py-1 border-[#363636] border-[1px] rounded-sm">
+          <button
+            className="w-full bg-[#0073E6] text-center text-[0.77rem] py-1 border-[#363636] border-[1px] rounded-sm"
+            onClick={validateData}
+          >
             Insert field
           </button>
         </div>
