8319ca4cdec98966bae1b5e563f02391be16fd68
rabbykhairul <rabbykhairul@gmail.com>
2023-10-26 11:23:23 +0600

    add onChange event to select input

--- src/components/form/SelectInput.tsx
+++ src/components/form/SelectInput.tsx
@@ -2,9 +2,10 @@ export interface SelectInputProps {
   label: string;
   options: { content: string; value: number | string }[];
   selectedValue: number | string;
+  onChange: (val: string) => void;
 }
 
-export default function SelectInput({ label, options, selectedValue }: SelectInputProps) {
+export default function SelectInput({ label, options, selectedValue, onChange }: SelectInputProps) {
   return (
     <div className="pb-2 m-0 flex flex-col gap-1">
       <span className="text-[0.77rem] box-border inline-block font-light text-[#ABABAB] m-0 p-0">{label}</span>
@@ -12,6 +13,7 @@ export default function SelectInput({ label, options, selectedValue }: SelectInp
       <select
         className="rounded-sm w-full px-[0.3rem] bg-[#2B2B2B] text-[0.7rem] leading-[1.1rem] p-1 focus:outline-none"
         value={selectedValue}
+        onChange={(e) => onChange(e.target.value)}
       >
         {options.map((op) => (
           <option key={op.value} value={op.value} selected={selectedValue === op.value}>
