2f80a2c34083a0b0dc1f5e3496a6d39af729b00e
rabbykhairul <rabbykhairul@gmail.com>
2023-10-26 11:31:33 +0600

    config for date picker languages

--- src/config/date.ts
+++ src/config/date.ts
@@ -28,3 +28,26 @@ export const WEEKDAYS: { value: number; content: string }[] = [
     value: 7,
   },
 ];
+
+export const DATE_PICKER_LANGUAGES: { value: string; content: string }[] = [
+  {
+    content: "English",
+    value: "en-US",
+  },
+  {
+    content: "Russian",
+    value: "ru-RU",
+  },
+  {
+    content: "French",
+    value: "fr-FR",
+  },
+  {
+    content: "Dutch",
+    value: "de-DE",
+  },
+  {
+    content: "Japanese",
+    value: "jp-JP",
+  },
+];
