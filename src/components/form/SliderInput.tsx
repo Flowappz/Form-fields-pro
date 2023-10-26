8e692dd3a095cd6ef14f355dbf0a9224952f8fd7
rabbykhairul <rabbykhairul@gmail.com>
2023-10-26 12:17:11 +0600

    slider input show current value

--- src/components/form/SliderInput.tsx
+++ src/components/form/SliderInput.tsx
@@ -11,7 +11,7 @@ export default function SliderInput({ label, max, min, value, onChange }: Slider
     <div className="pb-2 m-0 flex flex-col gap-1">
       <span className="text-[0.77rem] box-border inline-block font-light text-[#ABABAB] m-0 p-0">{label}</span>
 
-      <div className="w-full flex justify-between text-xs gap-3">
+      <div className="w-full flex justify-between items-center text-xs gap-3">
         <input
           type="range"
           min={min}
@@ -21,7 +21,7 @@ export default function SliderInput({ label, max, min, value, onChange }: Slider
           className="range accent-green-500 flex-1"
           step="1"
         />
-        <span>{String(value).padStart(2, "0")}</span>
+        <span className="p-1 bg-[#2B2B2B] font-bold">{String(value).padStart(2, "0")}</span>
       </div>
     </div>
   );
