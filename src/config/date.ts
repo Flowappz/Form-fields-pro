4c49c3b0a18277096e29d8ed5f56107419762ee0
rabbykhairul <rabbykhairul@gmail.com>
2023-10-26 11:19:32 +0600

    date config for weekdays

---
+++ src/config/date.ts
@@ -0,0 +1,30 @@
+export const WEEKDAYS: { value: number; content: string }[] = [
+  {
+    content: "Monday",
+    value: 1,
+  },
+  {
+    content: "Tuesday",
+    value: 2,
+  },
+  {
+    content: "Wednesday",
+    value: 3,
+  },
+  {
+    content: "Thursday",
+    value: 4,
+  },
+  {
+    content: "Friday",
+    value: 5,
+  },
+  {
+    content: "Saturday",
+    value: 6,
+  },
+  {
+    content: "Sunday",
+    value: 7,
+  },
+];
