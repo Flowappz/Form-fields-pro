614005a087804c701f644e02212d4d507c762be4
rabbykhairul <rabbykhairul@gmail.com>
2023-11-10 18:57:04 +0600

    dropdown form, collect hover bg color value from user

--- src/views/Dropdown.tsx
+++ src/views/Dropdown.tsx
@@ -4,6 +4,7 @@ import RemovableTextInput from "../components/form/RemovableTextInput";
 import { ZodError, z } from "zod";
 import { useAppContext } from "../contexts/AppContext";
 import * as webflowService from "../services/webflowService";
+import ColorInput from "../components/form/ColorInput";
 
 const inputSchema = z.object({
   dropdownLabel: z.string().min(1, "Please enter a label"),
@@ -21,6 +22,8 @@ export default function Dropdown() {
   const [dropdownLabel, setDropdownLabel] = useState("");
   const [inputFieldName, setInputFieldName] = useState("");
   const [dropdownItems, setDropdownItems] = useState<string[]>(new Array(3).fill(""));
+  const [lightHoverColor, setLightHoverColor] = useState("#aabbcc");
+  const [darkHoverColor, setDarkHoverColor] = useState("#aabbcc");
 
   const [errors, setErrors] = useState<any>({});
 
@@ -99,6 +102,13 @@ export default function Dropdown() {
           onChange={setInputFieldName}
           error={errors.inputFieldName}
         />
+
+        <ColorInput
+          label="Hover background color (Light theme)"
+          value={lightHoverColor}
+          onChange={setLightHoverColor}
+        />
+        <ColorInput label="Hover background color (Dark theme)" value={darkHoverColor} onChange={setDarkHoverColor} />
       </div>
 
       <div className="mt-[0.3rem]">
