81f30e59a5ab65b5b982a0d1140025cbfa34f446
rabbykhairul <rabbykhairul@gmail.com>
2023-11-22 23:11:40 +0600

    add background to no form selected state view

--- src/views/NoFormSelectedState.tsx
+++ src/views/NoFormSelectedState.tsx
@@ -7,16 +7,17 @@ export default function NoFormSelectedState() {
     if (form) return null;
 
     return (
-        <div className="w-full bg-[#2e2e2e] h-full flex items-center justify-center absolute top-0 left-0 z-10">
-            <div className="w-3/4 flex flex-col items-center p-[0.75rem] pb-[1.3rem] rounded">
-                <PinIcon color="white"/>
+      <div className="w-full bg-[#2e2e2e] h-full flex items-center justify-center absolute top-0 left-0 z-10">
+        <div className="w-3/4 bg-red-600 flex flex-col items-center p-[0.75rem] pb-[1.3rem] rounded">
+          <PinIcon color="white" />
 
-                <h4 className="text-center text-[#D9D9D9] font-bold text-[0.82rem] mt-[0.2rem] leading-5">Please select
-                    a form first</h4>
-                <p className="text-[0.77rem] text-[#ABABAB] font-light text-center">
-                    To use a form field, you must select a form in the designer console.
-                </p>
-            </div>
+          <h4 className="text-center text-[#D9D9D9] font-bold text-[0.82rem] mt-[0.2rem] leading-5">
+            Please select a form first
+          </h4>
+          <p className="text-[0.77rem] text-[#cdcccc] font-light text-center">
+            To use a form field, you must select a form in the designer console.
+          </p>
         </div>
+      </div>
     );
 }
