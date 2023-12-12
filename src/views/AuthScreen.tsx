f3d9861ef566370083e915187ec4483a51cb883f
rabbykhairul <rabbykhairul@gmail.com>
2023-12-12 08:31:37 +0600

    fix the auth button style

--- src/views/AuthScreen.tsx
+++ src/views/AuthScreen.tsx
@@ -5,7 +5,12 @@ export default function AuthScreen({ authUrl }: { authUrl: string }) {
         Failed to inject custom code! Perhaps you created this site after installing the app. In order to use the app
         with this site please authenticate again
       </p>
-      <a href={authUrl} target="_top">
+      <a
+        href={authUrl}
+        target="_top"
+        className="mt-10 boxShadows-action-colored mb-[60px] bg-[#0073E6] text-center text-[0.77rem] py-1 px-3 border-[#363636] border-[1px] rounded-[4px]"
+        onKeyDown={() => (window.parent.location.href = authUrl)}
+      >
         Authenticate
       </a>
     </div>
