107fe952efbfb60f6940e7b4420def7d0ca5f43a
rabbykhairul <rabbykhairul@gmail.com>
2023-12-14 21:18:32 +0600

    loading screen, add spinner

--- src/views/LoadingScreen.tsx
+++ src/views/LoadingScreen.tsx
@@ -2,7 +2,13 @@ import { PropsWithChildren } from "react";
 
 export default function LoadingScreen({ message }: PropsWithChildren<{ message: string }>) {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <div className="lds-ring mb-2">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <p>{message}</p>
     </div>
   );
